596dcfe968423348cd0ff9267f4877e293b2c46f
jamal-Bahammou <jamal.bahammou@ump.ac.ma>
2020-08-02 20:05:30 +0100

    CRUD api

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,9 +2,12 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-// First get request: get all the tours from api
+// READ DATA FROM JSON FILE
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+// GET ALL TOURS REQUEST --------------------------------------------------------------------------
 app.get('/api/v1/tours', ( req, res ) => {
     res.status(200).json({
         status: 'success',
@@ -12,6 +15,79 @@ app.get('/api/v1/tours', ( req, res ) => {
         data: {
             tours
         }
+    })
+})
+
+// GET SINGLE TOUR REQUEST ------------------------------------------------------------------------
+app.get('/api/v1/tours/:id', ( req, res ) => {
+    const id = req.params.id * 1;
+    const tour = tours.find( tour => tour.id === id )
+
+    if ( !tour ) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid id'
+        })
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour
+        }
+    })
+})
+
+// CREATE NEW TOUR REQUEST ------------------------------------------------------------------------
+app.post('/api/v1/tours', ( req, res ) => {
+    const newId = tours[tours.length-1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+})
+
+// UPDATE TOUR REQUEST ----------------------------------------------------------------------------
+app.patch('/api/v1/tours/:id', ( req, res ) => {
+    const id = req.params.id * 1;
+    const tour = tours.find( tour => tour.id === id )
+
+    if ( !tour ) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid id'
+        })
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: 'Updated tour here ...'
+        }
+    })
+})
+
+// DELETE TOUR REQUEST ----------------------------------------------------------------------------
+app.delete('/api/v1/tours/:id', ( req, res ) => {
+    const id = req.params.id * 1;
+    const tour = tours.find( tour => tour.id === id )
+
+    if ( !tour ) {
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Invalid id'
+        })
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
     })
 })
 
