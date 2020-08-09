23f55630f03750800606164be0ceeef3bd79f362
jamal-Bahammou <jamal.bahammou@ump.ac.ma>
2020-08-09 20:31:49 +0100

    Document Middlware & slugify

--- 4-natours/models/tourModel.js
+++ 4-natours/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const slugify = require('slugify')
 
 // CREATE THE SCHEMA
 const tourSchema = new mongoose.Schema({
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema({
         unique: true,
         trim: true
     },
+    slug: String,
     duration: {
         type: Number,
         required: [true, 'A tour must have a duration']
@@ -53,6 +55,20 @@ const tourSchema = new mongoose.Schema({
         select: false
     },
     startDates: [Date]
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+})
+
+// Virtual variable
+tourSchema.virtual('durationWeeks').get( function() {
+    return this.duration / 7;
+})
+
+// Document Middleware: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
